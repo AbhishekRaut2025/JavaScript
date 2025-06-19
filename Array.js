85d4d83122573312f37f4a0aee9b708d647623ef
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-19 19:39:20 +0530

    First Commit

---
+++ Array.js
@@ -0,0 +1,17 @@
+var arr = [1, 2, 3, 4]
+console.log(arr);
+
+arr.push(5);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
+arr.splice(2,2);
+console.log(arr);
