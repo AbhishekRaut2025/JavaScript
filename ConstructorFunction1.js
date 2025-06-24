9b86354d2245e7c0c7c2e13e0bd087fa895d9032
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-24 23:19:44 +0530

    Constructor Function

---
+++ ConstructorFunction1.js
@@ -0,0 +1,10 @@
+function Biscuit() {
+    this.width = 20;
+    this.height = 10;
+    this.colour = "Brown";
+    this.taste = "sweet";
+}
+var Biscuit1 = new Biscuit();      //use "new" keyword to call constructof function
+var Biscuit2 = new Biscuit();
+var Biscuit3 = new Biscuit();
+console.log(Biscuit1);
