36b0a3dbe435aae10ebc620b905e2d963862870c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-02 19:10:47 +0530

    New Code

--- IfElse.js
+++ IfElse.js
@@ -1,6 +1,21 @@
-if(1>0){
-    console.log("if")
+let marks =5;
+if (marks>100){
+    console.log("marks cannot be grater than 100");
+    return
+}
+else if(marks>75){
+    console.log("Distinction");
+    return
+}
+else if (marks>60){
+    console.log("First Class");
+    return
+}
+else if (marks>35){
+    console.log("Second Class");
+    return
 }
 else{
-    console.log("else")
+    console.log("Fail");
+    return
 }
