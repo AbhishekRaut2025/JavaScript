b1aa83139e5ae870db825824631a14cd9c43e0a8
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-03 21:25:31 +0530

    Addes new Function

--- RestOperator.js
+++ RestOperator.js
@@ -1,7 +1,12 @@
 function addition(...value) {
     console.log(value);
 }
-addition(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+addition(1, 2, 3, 4, 5);
 // rest operator turns a normal functions into dynamic functions.
 // any count of arguments can be passed
 // rest operator will store those values in array
+
+function abcd(a,b,c,...value){
+    console.log(a,b,c,value);
+}
+abcd(1,2,3,4,5,6,7);
