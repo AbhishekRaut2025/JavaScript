191337164d776e2ea5365a98c653da4c7b514c3a
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-03 17:59:04 +0530

    Loops Questions

---
+++ LoopsQuestion2.js
@@ -0,0 +1,6 @@
+//Print numbers from 10-1 using while loop
+let a = 10;
+while (a > 0) {
+    console.log(a);
+    a--;
+}
---
+++ LoopsQuestion3.js
@@ -0,0 +1,6 @@
+//Print even numbers fron 1-20 using for loop
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
