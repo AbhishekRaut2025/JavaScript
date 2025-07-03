bcb22782268b67354c900b4f46c572aeff6ecdad
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-03 21:05:40 +0530

    Rest Operator in Function

---
+++ RestOperator.js
@@ -0,0 +1,7 @@
+function addition(...value) {
+    console.log(value);
+}
+addition(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// rest operator turns a normal functions into dynamic functions.
+// any count of arguments can be passed
+// rest operator will store those values in array
