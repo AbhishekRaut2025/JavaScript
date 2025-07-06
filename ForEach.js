3cae8264c3003789450836b478e73e7e688c218a
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-06 20:04:09 +0530

    forEach operation on Array

---
+++ ForEach.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+arr.forEach(function(val){
+    console.log(val);
+})
+
+// in aboce case forEach array me se har ek value ke liye function chalata hai
+//for each targets every element of the array 
+//forEach always accepts a function
