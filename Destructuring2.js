60bec4f4b5360720658d8f257f60cb84bc99c8f5
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-07 20:03:16 +0530

    Destructuring using spread operator

---
+++ Destructuring2.js
@@ -0,0 +1,25 @@
+// DESTRUCTURING with operators
+let arr = [1, 2, 3, 4, 5];
+let phone = {
+    model: "Iphone",
+    brand: "Apple",
+    price: "42k",
+    colour: "Black"
+}
+
+// with spread(...) syntax/operator we can convert array into object
+let obj1 = { ...arr };
+console.log(obj1);
+
+
+// for function
+function sum(v1, v2, v3) {
+    let z = v1 + v2 + v3;
+    return z;
+}
+let a = sum(...arr);
+console.log(a)
+
+
+//we can use spread operatorn to print whole object
+console.log({ ...phone })
