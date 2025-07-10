b23b92ecfc723245b6a83d12933b5b464864b180
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-10 20:00:38 +0530

    added textContent, innerHTML, hidden feature

--- DOM/query-selector/script.js
+++ DOM/query-selector/script.js
@@ -1,3 +1,5 @@
-let h1 = document.querySelector("h1");
+let h1 = document.querySelector("h1");          //querySelector is to select the html element
 console.dir(h1);
-h1.textContent = "Hi Abhishek";
+h1.textContent = "Hi Abhishek";                 // chenged th orignal content
+h1.innerHTML = "<i>Hey</>"                      // changed tag/html using innerHTML key
+h1.hidden = true;                               // hides element from web page
