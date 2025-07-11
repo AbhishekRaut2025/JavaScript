dafcf4e57c1826e827d1b2999f515aa8825cdcdb
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-11 22:45:39 +0530

    Manipulation of style using JS

---
+++ DOM/style-updates/script.js
@@ -0,0 +1,7 @@
+// here we are chabging css using JS
+
+
+let firstH1 = document.querySelector("h1");
+console.log(firstH1);
+firstH1.style.color = "red"                     // changed colour   
+console.dir(firstH1);
