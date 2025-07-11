c96c64d3abb1a9cd8b391af4ded46dc85133ddff
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-11 18:15:00 +0530

    created a element using .createElement

---
+++ DOM/dynamic-DOM-manipulation/script.js
@@ -0,0 +1,5 @@
+// 1st ceate a element
+// 2nd append/prepend karo
+let h1 = document.createElement("h1");          // this create a h1 tag in html
+h1.textContent = "hey, How are you";
+document.body.prepend(h1);                      // this shows our created tag on page
