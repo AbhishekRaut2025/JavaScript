1a078f5dfaa5bd2abe6e9e8b1ba0b6ce8ad36206
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-12 23:07:12 +0530

    Get src of image using JS

---
+++ DOM/Attribute-manipulation/Question 1/script.js
@@ -0,0 +1,4 @@
+// How do you get src ff image using JS
+let img = document.querySelector("img");
+console.log(img.getAttribute("src"));       //1st way to print
+console.log(img.src);                       //2nd way to print
