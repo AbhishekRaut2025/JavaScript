bf519b560ee9c8295bd04b7469b44a3867847c99
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-14 22:51:04 +0530

    print input to console

--- DOM/event-listeners/Input-event/script.js
+++ DOM/event-listeners/Input-event/script.js
@@ -1,6 +1,9 @@
 // input event listener is when a user gives input may bi in input box it triggers an event
 
 let userInput = document.querySelector("input");
-userInput.addEventListener("input", function(){
-        console.log("Typed");
-})
+userInput.addEventListener("input", function(details){         //we can accept data from the event to this function
+        // console.log(details.data);                          //print this input data to console
+        if(details.data !== null){                             //when wedo backspace in input field is considers it as null    
+            console.log(details.data);                         //and prints to console to avoid that we set a condition
+        }
+}) 
