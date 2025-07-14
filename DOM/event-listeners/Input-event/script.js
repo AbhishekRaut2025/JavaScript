55dd55c9ea54ee8f2aa8caf00820526dd246127c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-14 20:34:40 +0530

    input event

---
+++ DOM/event-listeners/Input-event/script.js
@@ -0,0 +1,6 @@
+// input event listener is when a user gives input may bi in input box it triggers an event
+
+let userInput = document.querySelector("input");
+userInput.addEventListener("input", function(){
+        console.log("Typed");
+})
