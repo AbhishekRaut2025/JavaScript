ab3634b500109e6763ae5f678a950ccf8515ccdd
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-16 23:45:02 +0530

    Download button progress bar

---
+++ DOM/Timers - Intervals/download-bar/script.js
@@ -0,0 +1,19 @@
+let progressText = document.querySelector("span");
+let button = document.querySelector("#startBtn");
+let progressBar = document.querySelector(".progress");
+
+button.addEventListener("click", function () {
+    let count = 0;
+    progressBar.style.width = "0%";
+    progressText.textContent = "0%";
+    let interval = setInterval(function () {
+        if (count <= 100) {
+            progressText.textContent = `${count} %`
+            progressBar.style.width = `${count}%`
+
+            count++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 300)
+})
