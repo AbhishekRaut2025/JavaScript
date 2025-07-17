edb9667f15286447cf9231fce92419b09acffbb3
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-17 19:43:42 +0530

    Coockies

---
+++ DOM/Browser Storage/Coockies/script.js
@@ -0,0 +1,10 @@
+// Coockies are used ot store small dala in browser (~4kb)
+// sessionStorage & localStorage stores about ~5MB
+
+
+
+// data stored in coockies will be sent to server autometically when page reloded
+
+
+document.cookie = "Email = abhishek@gmail.com;"     //Stored data in coockie
+document.cookie = "age = 24"                        //Stored data in coockie
