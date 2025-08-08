590de424333cda7f84d539ba5982486e379c837e
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-08 22:49:16 +0530

    Block scope

---
+++ basics/3.0/scope/blockScope.js
@@ -0,0 +1,12 @@
+// Block scope
+{
+  let a = 10;
+  const b = 20;
+  console.log(a);
+  console.log( b);
+}
+
+console.log(a);
+console.log(b);
+
+// here we cannot access the variable outside the scope of the block
