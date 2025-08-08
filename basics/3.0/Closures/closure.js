07cc0cc9469f7dc051cefecef990a0a220848d6c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-08 23:54:00 +0530

    Closures

---
+++ basics/3.0/Closures/closure.js
@@ -0,0 +1,18 @@
+// Closures
+
+function outer() {
+  let count = 0;
+
+  function inner() {
+    count++;
+    console.log(count);
+  }
+
+  return inner;
+}
+
+const counter = outer();
+
+counter(); // 1
+counter(); // 2
+counter(); // 3
